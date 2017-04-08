673291ab546fc6bd01b09b719b63be7deb16b5e0
Leo SIMON <lsimon@e2r12p10.42.fr>
2017-04-08 17:33:11 +0200

    inscription ok

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>ECOMMERCE</title>
+</head>
+<body>
+  <?php include("includes/header.php");?>
+</body>
+</html>
---
+++ sign_up.php
@@ -0,0 +1,33 @@
+<?php
+	function check_mail($db, $mail)
+	{
+		if (!($mails = mysqli_query($db, "SELECT `email` FROM `client` WHERE `email` = '$mail'")))
+			return (FALSE);
+		if (mysqli_num_rows($mails) > 0)
+			return (FALSE);
+		return (TRUE);
+	}
+
+	$error = FALSE;
+	$db = mysqli_connect("localhost","root", "root", "market");
+	if (mysqli_connect_errno())
+	{
+		echo "Connection error\n";
+		exit;
+	}
+	if ($_POST["submit"] == "OK" && $_POST["mail"] && $_POST["passwd"])
+	{
+		$mail = $_POST['mail'];
+		$pass = hash("whirlpool", $_POST['passwd']);
+		if (!check_mail($db, $mail))
+		{
+			header('Location: /rush00/error.html');
+			echo ("This email already exists\n");
+		}
+		else
+			if (!mysqli_query($db, "INSERT INTO `client`(`email`, `mdp`) VALUES ('$mail', '$pass')") === TRUE)
+				echo "ko\n";
+	}
+	else
+		echo "ERROR\n";
+?>
