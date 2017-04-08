f5c023d47342d15df74b4993a992014739227122
Leo SIMON <lsimon@e2r12p10.42.fr>
2017-04-08 17:51:45 +0200

    includes ok

---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #111;
+}
+</style>
+</head>
+<body>
+  <header>
+     <h1><center>Fruits Légumes &amp; co.</center></h1>
+  </header>
+<ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="sign_up.html">Inscription</a></li>
+  <li><a href="connect.html">Me connecter</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">Panier</a></li>
+</ul>
+
+</body>
+</html>
