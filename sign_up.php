97f90d110647c1353c02f7d234868bf90e884106
Benoit CORBEL <becorbel@e2r12p8.42.fr>
2017-04-09 21:42:35 +0200

    ajout

--- sign_up.php
+++ sign_up.php
@@ -11,7 +11,7 @@
 	<h1 id="se-connecter">Inscription</h1>
 	<form action="sign_up_db.php" method="post">
 		<span id="text-mail">Email : </span><input id="mail" type="email" name="mail"><br>
-		<span id="text-passwd">Mot de passe : </span><input id="passwd" type="password" name="passwd">
+		<span id="text-passwd">Mot de passe : </span><input id="passwd" type="password" name="passwd" required>
 		<input id="submit" type="submit" name="submit" value="OK"><br>
 		<a id="retour" href="/rush00/index.php">Retour</a>
 	</form>
