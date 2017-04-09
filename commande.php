949f577a4fbca65188da90a4b5c7c28c3847e60d
Benoit CORBEL <becorbel@e2r12p8.42.fr>
2017-04-09 19:26:08 +0200

    ajout

--- commande.php
+++ commande.php
@@ -1,5 +1,13 @@
 <?PHP
 include("./includes/header.php");
-echo "Commande termine, merci d'avoir utilise legumes and co.";
-
+//echo "Commande termine, merci d'avoir utilise legumes and co.\n";
+$db = mysqli_connect("localhost", "root", "root", "market");
+foreach($_SESSION['panier'] as $key =>$value)
+{
+	$id_client = mysqli_query($db, "SELECT `id` FROM client WHERE email='".$_SESSION['user']."'");
+	$result = mysqli_fetch_array($id_client, MYSQLI_ASSOC);
+	echo $result[0];
+	//$query = mysqli_query($db, "INSERT INTO commande (`id_client`, `id_produit`, `quantite`, `total`) VALUES ('". $result"', '" $key"', '"$value)"', );
+	
+}
 ?>
