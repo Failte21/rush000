6dc9c95674866b749165690f634bfe1d27d14b74
Franck PIPART <fpipart@e2r11p9.42.fr>
2017-04-09 12:20:42 +0200

    new feature cat

--- includes/categories.php
+++ includes/categories.php
@@ -5,7 +5,7 @@
  <style>
 .green {
      background-color: #32CD32;
-     max-width: 160px;
+     max-width: 580px;
  }
  </style>
  </head>
@@ -13,6 +13,10 @@
  <ul class="green">
    <li><a href="?Categorie=fruits">Fruits</a></li>
    <li><a href="?Categorie=legumes">Légumes</a></li>
+   <li><a href="?Categorie=boulangerie">Boulangerie</a></li>
+   <li><a href="?Categorie=poissonnerie">Poissonnerie</a></li>
+   <li><a href="?Categorie=boucherie">Boucherie</a></li>
+   <li><a href="?Categorie=conserve">Conserve</a></li>
  </ul>
  </body>
  </html>
